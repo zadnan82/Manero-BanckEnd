3a02b45171c7302d5a55e734b5d62fb7ff91c448
zadnan82 <90682601+zadnan82@users.noreply.github.com>
2023-11-01 23:09:58 +0100

    Revert "Skapat AuthController Register/Login"

--- Manero-BanckEnd/Program.cs
+++ Manero-BanckEnd/Program.cs
@@ -1,33 +1,25 @@
 
 
-using Manero_BanckEnd.Contexts;
-using Manero_BanckEnd.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
-   
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
-builder.Services.AddTransient<IAuthService, AuthService>();
- 
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger(c => { c.RouteTemplate = "swagger/{documentName}/swagger.json"; });
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API v1");
+    c.RoutePrefix = string.Empty;
+});
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 
 app.UseHttpsRedirection();
-app.UseAuthentication();
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
--- Manero-BanckEnd/Schemas/LoginSchema.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Manero_BanckEnd.Schemas;
-
-public class LoginSchema
-{
-    public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
-
-    //public bool RememberMe { get; set; } = false;
-}
