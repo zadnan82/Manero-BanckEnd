6244ff0b18c198b8e98c42d0308610a35c24fb0c
zadnan82 <zadnaniths@gmail.com>
2023-11-08 17:52:31 +0100

    Added token to login

--- Manero-BanckEnd/Program.cs
+++ Manero-BanckEnd/Program.cs
@@ -1,5 +1,4 @@
-
-
+using Manero_BanckEnd;
 using Manero_BanckEnd.Contexts;
 using Manero_BanckEnd.Helpers;
 using Manero_BanckEnd.Repositories;
@@ -15,8 +14,39 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
    
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddTransient<TokenGenerator>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(x=>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Token:Issuer"],
+            ValidAudience = builder.Configuration["Token:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:Key"]!)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+builder.Services.AddSwaggerGen(sw =>
+{
+    sw.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "Manero API",
+        Description = @"API for Manero",
+        Contact = new OpenApiContact
+        {
+            Name = "Group 3",
+
+        },
+    });
+
+});
+builder.Services.AddAuthorization();
+
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<UserService>();
@@ -26,19 +56,11 @@ builder.Services.AddScoped<ApiKeyRepo>();
 builder.Services.AddScoped<UseApiKeyAttribute>();
 
 
-
-
-
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 app.UseCors(x=> x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
---
+++ Manero-BanckEnd/TokenGenerator.cs
@@ -0,0 +1,66 @@
+﻿using Manero_BanckEnd.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.CodeDom.Compiler;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Manero_BanckEnd
+{
+    public class TokenGenerator
+    {
+         
+            private readonly IConfiguration _configuration;
+
+        public TokenGenerator(IConfiguration configuration)
+        { 
+            _configuration = configuration;
+        }
+
+
+        public string Generate()
+        {
+
+          
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:Key"]!));
+            var signingKey = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+
+            var securityToken = new JwtSecurityToken
+                (
+                    _configuration["Token:Issuer"],
+                    _configuration["Token:Audience"],
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signingKey
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(securityToken);
+            return token;
+        }
+
+        public string Generate(Claim[] claims )
+        { 
+
+            //var claims = new Claim[]
+            //    {
+            //            new Claim(ClaimTypes.Email, user.Email),
+            //            new Claim(ClaimTypes.Name, user.Email),
+            //            new Claim("ApiKey", _configuration["ApiKeys:User"]!)
+            //    };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Token:Key"]!));
+            var signingKey = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var securityToken = new JwtSecurityToken
+                (
+                    _configuration["Token:Issuer"],
+                    _configuration["Token:Audience"],
+                    claims,
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signingKey
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(securityToken);
+            return token;
+        }
+    }
+}
