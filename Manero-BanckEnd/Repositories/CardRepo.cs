67a9c111a47a77d09d218f8fba3b4df32a818353
OJin3D <113429005+OJin3D@users.noreply.github.com>
2023-11-14 13:22:09 +0100

    CardRepo created

---
+++ Manero-BanckEnd/Repositories/CardRepo.cs
@@ -0,0 +1,32 @@
+﻿using Manero_BanckEnd.Contexts;
+using Manero_BanckEnd.Entities;
+
+namespace Manero_BanckEnd.Repositories;
+
+public class CardRepo : Repo<CardEntity>
+{
+    private readonly DataContext _context;
+    public CardRepo(DataContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<CardEntity> GetAsync(int cardId, string userId)
+    {
+        return await _context.Cards.Where(x => x.UserId == userId && x.Id == cardId).FirstOrDefaultAsync();
+    }
+
+    public async Task<ICollection<CardEntity>> GetUserByCardDetailsAsync(string userId)
+    {
+        return await _context.Cards.Where(x => x.UserId == userId).ToListAsync();
+    }
+
+    public async Task<string> GetUserIdByEmailAsync(string userEmail)
+    {
+
+        var user = await _context.Users.Where(u => u.Email == userEmail).FirstOrDefaultAsync();
+
+
+        return user?.Id;
+    }
+}
