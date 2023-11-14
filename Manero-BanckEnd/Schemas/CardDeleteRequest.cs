f7319e8727a56b4e65013f467e349bec1f4117a3
OJin3D <113429005+OJin3D@users.noreply.github.com>
2023-11-14 13:19:57 +0100

    CardDeleteRequest created

---
+++ Manero-BanckEnd/Schemas/CardDeleteRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Manero_BanckEnd.Schemas;
+
+public class CardDeleteRequest
+{
+    public int CardId { get; set; }
+
+    public static implicit operator int(CardDeleteRequest request)
+    {
+        return request.CardId;
+    }
+}
