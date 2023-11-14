e8572a580e461ee9906359355095247483021c97
OJin3D <113429005+OJin3D@users.noreply.github.com>
2023-11-14 13:20:50 +0100

    CardUpdateRequest created

---
+++ Manero-BanckEnd/Schemas/CardUpdateRequest.cs
@@ -0,0 +1,28 @@
+﻿using Manero_BanckEnd.Entities;
+
+namespace Manero_BanckEnd.Schemas;
+
+public class CardUpdateRequest
+{
+    public int Id { get; set; }
+
+    public string CardNameHolder { get; set; } = null!;
+
+    public int CardNumber { get; set; }
+
+    public int CVV { get; set; }
+
+    public DateTime ExpireDate { get; set; }
+
+    public static implicit operator CardEntity(CardUpdateRequest request)
+    {
+        return new CardEntity()
+        {
+            Id = request.Id,
+            CardHolderName = request.CardNameHolder,
+            CardNumber = request.CardNumber,
+            CVV = request.CVV,
+            ExpirationDate = request.ExpireDate,
+        };
+    }
+}
