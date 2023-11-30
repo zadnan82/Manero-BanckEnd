f10e119d5badc23c8cffc64ed4518ad4bc34f93d
OJin3D <113429005+OJin3D@users.noreply.github.com>
2023-11-30 14:31:38 +0100

    promocode repository added

--- Manero-BanckEnd/Repositories/PromoCodeRepository.cs
+++ Manero-BanckEnd/Repositories/PromoCodeRepository.cs
@@ -1,6 +1,44 @@
-﻿namespace Manero_BanckEnd.Repositories
+﻿using Manero_BanckEnd.Contexts;
+
+namespace Manero_BanckEnd.Repositories;
+
+public abstract class PromoCodeRepository<TEntity> where TEntity : class
 {
-    public class PromoCodeRepository
+    private readonly DataContext _dataContext;
+
+    protected PromoCodeRepository(DataContext dataContext)
     {
+        _dataContext = dataContext;
+    }
+
+    public virtual async Task<TEntity> AddAsync(TEntity entity)
+    {
+        if (entity != null)
+        {
+            _dataContext.Set<TEntity>().Add(entity);
+            await _dataContext.SaveChangesAsync();
+            return entity;
+        }
+        return null!;
+    }
+
+
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+        return await _dataContext.Set<TEntity>().ToListAsync();
+    }
+
+    public virtual async Task<TEntity> DeleteAsync(int id)
+    {
+        var entity = await _dataContext.Set<TEntity>().FindAsync(id);
+
+        if (entity != null)
+        {
+            _dataContext.Set<TEntity>().Remove(entity);
+            await _dataContext.SaveChangesAsync();
+            return null!;
+        }
+
+        return null!;
     }
 }
