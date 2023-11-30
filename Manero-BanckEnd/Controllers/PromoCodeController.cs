ca1babea715989fc1e570ba7812c0d3d8162c74d
OJin3D <113429005+OJin3D@users.noreply.github.com>
2023-11-30 14:27:05 +0100

    Promocode controller added

---
+++ Manero-BanckEnd/Controllers/PromoCodeController.cs
@@ -0,0 +1,40 @@
+﻿using Manero_BanckEnd.Schemas.PromoCodes;
+using Manero_BanckEnd.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manero_BanckEnd.Controllers;
+
+[Route("api/PromoCode")]
+[ApiController]
+public class PromoCodeController : ControllerBase
+{
+    private readonly PromoCodeService _promoCodeService;
+
+    public PromoCodeController(PromoCodeService promoCodeService)
+    {
+        _promoCodeService = promoCodeService;
+    }
+
+
+    [HttpGet("GETALL")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var list = await _promoCodeService.GetAllAsync();
+        return StatusCode(200, list);
+    }
+
+    [HttpDelete("DELETE")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var promoCode = await _promoCodeService.DeleteAsync(id);
+        return StatusCode(200, promoCode);
+    }
+
+    [HttpPost("CREATE")]
+    public async Task<IActionResult> CreateAsync(PromoCodeSchema promoCodeSchema)
+    {
+        var promoCode = await _promoCodeService.CreateAsync(promoCodeSchema);
+        return StatusCode(201, promoCode);
+    }
+}
