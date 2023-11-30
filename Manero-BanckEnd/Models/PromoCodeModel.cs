5b7e91a91189e656fed2296409f5bfebf5db894a
OJin3D <113429005+OJin3D@users.noreply.github.com>
2023-11-30 14:11:38 +0100

    promocode model added

---
+++ Manero-BanckEnd/Models/PromoCodeModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Manero_BanckEnd.Models;
+
+public class PromoCodeModel
+{
+    public int Id { get; set; }
+
+    public string PromoName { get; set; } = null!;
+
+    public double SalePercentage { get; set; }
+
+    public DateTime Validity { get; set; }
+}
